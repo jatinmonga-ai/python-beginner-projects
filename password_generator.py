83841e00dd9834cdf61d1fcfefda2c36bac9f8a6
jatinmonga-ai <jatinmonga2000@gmail.com>
2025-12-04 19:51:05 +0530

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,3 +1,10 @@
 import random
 import string
-length = int(input("Enter password length: "))
+
+define generate_password(length):
+       characters= string.ascii_letter + string.digits + string.punctuation
+       password= ' '.join(random.choice(characters) for_in range(length))
+       return password
+
+password_length = int(input("Enter password length: "))
+print("Your generated password is : ", generate_password(password_length)
